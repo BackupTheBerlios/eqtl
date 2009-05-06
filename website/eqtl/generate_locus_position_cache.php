8321abc89bb10b4bf89da9cf398362d018a07faf
Hajo Nils Krabbenhöft <fxtentacle@HajosMacOsX.local>
2009-05-06 15:42:56 +0200

    this php file will create a table containing the position in basepairs for each locus. it will use the func_conversion.php to convert from the cMorgan positions stored in locus table

--- website/eqtl/generate_locus_position_cache.php
+++ website/eqtl/generate_locus_position_cache.php
@@ -2,8 +2,8 @@
 require_once("func_conversion.php");
 mysql_connect("localhost","root","");
 mysql_select_db("eQTL_Stockholm");
-mysql_query("DROP TABLE IF EXISTS hajo_locus_to_position");
-mysql_query("CREATE TABLE hajo_locus_to_position (
+mysql_query("DROP TABLE IF EXISTS cache_locus_to_position");
+mysql_query("CREATE TABLE cache_locus_to_position (
   locus_id int(11) NOT NULL,
   positionBP int(11) NOT NULL,
   PRIMARY KEY  (locus_id)
@@ -16,6 +16,6 @@ while ($row = mysql_fetch_assoc($result)) {
     echo $row["cMorgan"];
     $pos = cM2bp($row["Chr"],  $row["cMorgan"]);
     echo " ".$pos."<br>\n";
-    mysql_query("INSERT INTO hajo_locus_to_position SET locus_id='".$row["No"]."', positionBP='".$pos."' ");
+    mysql_query("INSERT INTO cache_locus_to_position SET locus_id='".$row["No"]."', positionBP='".$pos."' ");
 }
 ?>
