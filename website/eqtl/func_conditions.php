9890e1d310a202763742068ab07bba7ff54face6
Steffen Möller <moeller@inb.uni-luebeck.de>
2009-06-23 11:43:04 +0200

    Added documentation.

--- website/eqtl/func_conditions.php
+++ website/eqtl/func_conditions.php
@@ -1,5 +1,31 @@
 
 <?php
+
+/**
+
+=head1 NAME
+
+func_conditions.php - management of lists of constraits
+
+=head1 SYNOPSIS
+
+to be included once by PHP scripts that show constraints to be selected from
+
+=head1 DESCRIPTION
+
+When attributes are queried against that are shared between forms,
+then the associated code should move into this file. The constraint themselves
+are defined as an array of "name","SQL code" pairs. This shall help to reduce
+the amount of redundant code between web forms.
+
+=head1 AUTHORS
+
+Steffen Moeller <moeller@inb.uni-luebeck.de>
+
+=cut
+
+*/
+
 function print_condition_form_element($conditionList,$prompt,$condition) {
 	if (empty($condition)) {
 		$condition=array();
@@ -13,4 +39,5 @@ function print_condition_form_element($conditionList,$prompt,$condition) {
 	echo "</table>\n";
 	echo "</p>";
 }
-	?>
+
+?>
