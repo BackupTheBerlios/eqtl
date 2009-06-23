f751b6cb231c4ad93723f85eb3fc07cbb3b295b2
Steffen Möller <moeller@inb.uni-luebeck.de>
2009-06-23 11:50:02 +0200

    Sharing constraint presentation with qtl.php
    Initial steps.

--- website/eqtl/func_conditions.php
+++ website/eqtl/func_conditions.php
@@ -34,7 +34,7 @@ function print_condition_form_element($conditionList,$prompt,$condition) {
 	echo "<table>";
 	foreach($conditionList as $n=>$c) {
 		//print_r($c);
-		echo "<tr><td><input type=checkbox name=condition[] value=\"$n\"> $n :</td><td><i>".$c["description"]."</i></td</tr>\n";
+		echo "<tr><td nowrap valign=top><input type=checkbox name=condition[] value=\"$n\"> $n :</td><td><i>".$c["description"]."</i></td</tr>\n";
 	}
 	echo "</table>\n";
 	echo "</p>";
