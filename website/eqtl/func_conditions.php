44a3ef6150284f85b6e8abac475810e794fceb52
Steffen Möller <moeller@inb.uni-luebeck.de>
2009-07-03 01:35:39 +0200

    Can now select for cis- and trans QTL groups

--- website/eqtl/func_conditions.php
+++ website/eqtl/func_conditions.php
@@ -26,15 +26,32 @@ Steffen Moeller <moeller@inb.uni-luebeck.de>
 
 */
 
-function print_condition_form_element($conditionList,$prompt,$condition) {
+function print_condition_form_element($conditionList,$prompt,$condition,$radioOrCheckbox="checkbox") {
 	if (empty($condition)) {
 		$condition=array();
 	}
 	echo "<p>$prompt<br>";
 	echo "<table>";
+	if ("radio"=="$radioOrCheckbox") {
+		echo "<tr>";
+		echo "<td nowrap valign=top><input type=$radioOrCheckbox name=condition[] value=\"\"";
+		if (0==count($condition)) echo " checked";
+		echo ">";
+		echo " neither :</td><td><i>don't perform</i></td>";
+		echo "</tr>\n";
+	}
+	elseif ("checkbox"!="$radioOrCheckbox") {
+		errorMessage("print_condition_form_element: unknown type '$radioOrCheckbox'.");
+		exit;
+	}
 	foreach($conditionList as $n=>$c) {
 		//print_r($c);
-		echo "<tr><td nowrap valign=top><input type=checkbox name=condition[] value=\"$n\"> $n :</td><td><i>".$c["description"]."</i></td</tr>\n";
+		echo "<tr>";
+		echo "<td nowrap valign=top><input type=$radioOrCheckbox name=condition[] value=\"$n\"";
+		if (in_array($n,$condition)) echo " checked";
+		echo ">";
+		echo " $n :</td><td><i>".$c["description"]."</i></td>";
+		echo "</tr>\n";
 	}
 	echo "</table>\n";
 	echo "</p>";
