0dfda9959f720e0a3eba4abdbd0776baa9d7da7b
Hajo Nils Krabbenhöft <fxtentacle@HajosMacOsX.local>
2009-07-04 18:03:59 +0200

    working on M BP Box

--- src/de/uni_luebeck/inb/krabbenh/entities/MillionBasepairBox_StatisticsAll.java
+++ src/de/uni_luebeck/inb/krabbenh/entities/MillionBasepairBox_StatisticsAll.java
@@ -10,64 +10,84 @@ import javax.persistence.Table;
 import org.hibernate.annotations.Immutable;
 
 @Entity
-@Table(name="millionbasepairbox_statistics_all")
+@Table(name = "millionbasepairbox_statistics_all")
 @Immutable
 public class MillionBasepairBox_StatisticsAll implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private int id;
-	
+
 	private int eqtlCount;
-	
+
 	private double lodAverage;
 	private double lodMin;
 	private double lodMax;
-	private double lodStdDev;
-	
+	private Double lodStdDev; // may be null
+
 	private double frequencySameChromosome;
 
-	
 	@Id
-	@Column(name="millionbasepairbox_id")
+	@Column(name = "millionbasepairbox_id")
 	public int getId() {
 		return id;
 	}
 
-
-	@Column(name="eqtlcount")
+	@Column(name = "eqtlcount")
 	public int getEqtlCount() {
 		return eqtlCount;
 	}
 
-
-	@Column(name="AVG_lod")
+	@Column(name = "AVG_lod")
 	public double getLodAverage() {
 		return lodAverage;
 	}
 
-
-	@Column(name="MIN_lod")
+	@Column(name = "MIN_lod")
 	public double getLodMin() {
 		return lodMin;
 	}
 
-
-	@Column(name="MAX_lod")
+	@Column(name = "MAX_lod")
 	public double getLodMax() {
 		return lodMax;
 	}
 
-
-	@Column(name="STDDEV_lod")
-	public double getLodStdDev() {
+	@Column(name = "STDDEV_lod")
+	public Double getLodStdDev() {
 		return lodStdDev;
 	}
 
-
-	@Column(name="AVG_samechromosome")
+	@Column(name = "AVG_samechromosome")
 	public double getFrequencySameChromosome() {
 		return frequencySameChromosome;
 	}
 
+	protected void setId(int id) {
+		this.id = id;
+	}
+
+	protected void setEqtlCount(int eqtlCount) {
+		this.eqtlCount = eqtlCount;
+	}
+
+	protected void setLodAverage(double lodAverage) {
+		this.lodAverage = lodAverage;
+	}
+
+	protected void setLodMin(double lodMin) {
+		this.lodMin = lodMin;
+	}
+
+	protected void setLodMax(double lodMax) {
+		this.lodMax = lodMax;
+	}
+
+	protected void setLodStdDev(Double lodStdDev) {
+		this.lodStdDev = lodStdDev;
+	}
+
+	protected void setFrequencySameChromosome(double frequencySameChromosome) {
+		this.frequencySameChromosome = frequencySameChromosome;
+	}
 
 }
--- src/de/uni_luebeck/inb/krabbenh/entities/MillionBasepairBox_StatisticsCis.java
+++ src/de/uni_luebeck/inb/krabbenh/entities/MillionBasepairBox_StatisticsCis.java
@@ -10,74 +10,134 @@ import javax.persistence.Table;
 import org.hibernate.annotations.Immutable;
 
 @Entity
-@Table(name = "millionbasepairbox_statistics_cis")
+@Table(name="millionbasepairbox_statistics_cis")
 @Immutable
 public class MillionBasepairBox_StatisticsCis implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private int id;
-
+	
 	private int eqtlCount;
-
+	
 	private double lodAverage;
 	private double lodMin;
 	private double lodMax;
-	private double lodStdDev;
+	private Double lodStdDev; // may be null
 
 	private double distanceAverage;
 	private double distanceMin;
 	private double distanceMax;
-	private double distanceStdDev;
+	private Double distanceStdDev; // may be null
 
+	
 	@Id
-	@Column(name = "millionbasepairbox_id")
+	@Column(name="millionbasepairbox_id")
 	public int getId() {
 		return id;
 	}
 
-	@Column(name = "eqtlcount")
+
+	@Column(name="eqtlcount")
 	public int getEqtlCount() {
 		return eqtlCount;
 	}
 
-	@Column(name = "AVG_lod")
+
+	@Column(name="AVG_lod")
 	public double getLodAverage() {
 		return lodAverage;
 	}
 
-	@Column(name = "MIN_lod")
+
+	@Column(name="MIN_lod")
 	public double getLodMin() {
 		return lodMin;
 	}
 
-	@Column(name = "MAX_lod")
+
+	@Column(name="MAX_lod")
 	public double getLodMax() {
 		return lodMax;
 	}
 
-	@Column(name = "STDDEV_lod")
-	public double getLodStdDev() {
+
+	@Column(name="STDDEV_lod")
+	public Double getLodStdDev() {
 		return lodStdDev;
 	}
 
-	@Column(name = "AVG_distance")
+
+
+	@Column(name="AVG_distance")
 	public double getDistanceAverage() {
 		return distanceAverage;
 	}
 
-	@Column(name = "MIN_distance")
+
+	@Column(name="MIN_distance")
 	public double getDistanceMin() {
 		return distanceMin;
 	}
 
-	@Column(name = "MAX_distance")
+
+	@Column(name="MAX_distance")
 	public double getDistanceMax() {
 		return distanceMax;
 	}
 
-	@Column(name = "STDDEV_distance")
-	public double getDistanceStdDev() {
+
+	@Column(name="STDDEV_distance")
+	public Double getDistanceStdDev() {
 		return distanceStdDev;
 	}
 
+
+	protected void setId(int id) {
+		this.id = id;
+	}
+
+
+	protected void setEqtlCount(int eqtlCount) {
+		this.eqtlCount = eqtlCount;
+	}
+
+
+	protected void setLodAverage(double lodAverage) {
+		this.lodAverage = lodAverage;
+	}
+
+
+	protected void setLodMin(double lodMin) {
+		this.lodMin = lodMin;
+	}
+
+
+	protected void setLodMax(double lodMax) {
+		this.lodMax = lodMax;
+	}
+
+
+	protected void setLodStdDev(Double lodStdDev) {
+		this.lodStdDev = lodStdDev;
+	}
+
+
+	protected void setDistanceAverage(double distanceAverage) {
+		this.distanceAverage = distanceAverage;
+	}
+
+
+	protected void setDistanceMin(double distanceMin) {
+		this.distanceMin = distanceMin;
+	}
+
+
+	protected void setDistanceMax(double distanceMax) {
+		this.distanceMax = distanceMax;
+	}
+
+
+	protected void setDistanceStdDev(Double distanceStdDev) {
+		this.distanceStdDev = distanceStdDev;
+	}
 }
