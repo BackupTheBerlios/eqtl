be48727dcef88035c08a7d080f1ee40e42f18ab3
Steffen Möller <moeller@inb.uni-luebeck.de>
2009-09-25 22:48:57 +0200

    scatter now integrated with website

--- website/eqtl/scatter_wrapper.sh
+++ website/eqtl/scatter_wrapper.sh
@@ -7,7 +7,8 @@ echo "Arguments are: $*"
 if [ ! -x $pathToHere/../../scripts/analyses/scatter_all.R ]; then
 	echo "Could not find Script '$pathToHere/../../scripts/analyses/scatter_all.R' expected from `pwd`."
 else
-	echo "Invocation of scatter_all.R"
+	echo "Invocation of scatter_all.R:"
+	echo "$pathToHere/../../scripts/analyses/scatter_all.R $*"
 	$pathToHere/../../scripts/analyses/scatter_all.R $*
 	echo "Invocation completed."
 fi
