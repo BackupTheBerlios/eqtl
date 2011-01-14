41f5615a8bb7f586551df9baf51a5947402bcc4d
Steffen Möller <steffen_moeller@gmx.de>
2011-01-14 16:39:55 +0100

    Version for preparing first papers
    Ralf Ludwig
    Robert Jaster

--- scripts/happy/01_prepareData.R
+++ scripts/happy/01_prepareData.R
@@ -11,8 +11,8 @@ data.prepare<-FALSE
 data.binary<-F
 data.covariates<-NULL
 #data.covariates<-c("sex","weight.6m")
-#model<-"additive"
-model<-"full"
+model<-"additive"
+#model<-"full"
 split.chromosomes<-F
 generations<-4
 
@@ -35,9 +35,10 @@ if (!file.exists(outputdir)) {
 	dir.create(outputdir)
 }
 
-permute<-1000
+#permute<-1000
+permute<-0
 
-
+verbose<-F
 
 #  G E N O T Y P E S
 
@@ -429,9 +430,65 @@ analyse.split.chromosomes<-function(phen,chr) {
 	}
 }
 
+analyse.all.chromosomes.together<-function(phen,individuals.subset,data.covariates,name.suffix="",verbose=FALSE,vlines.chr.col="darkgreen") {
+	cat("\n")
+	covariates.suffix<-paste("_covars_",ifelse(is.null(data.covariates),"none",paste(data.covariates,collapse=",",sep="")),sep="")
+	cat("Investigating",phen,name.suffix,"at times",individuals.subset,"and",covariates.suffix,"\n")
+
+	if ("weight.6m" %in% data.covariates && "6m" != individuals.subset) {
+		cat("weight.6m specified as covariate, need to work also with that subset.\n")
+		return(FALSE);
+	}
+	if ("weight.3m" %in% data.covariates && "3m" != individuals.subset) {
+		cat("weight.3m specified as covariate, need to work also with that subset.\n")
+		return(FALSE);
+	}
+	
+	fname<-paste("happy_",individuals.subset,"_",phen,name.suffix,".input",sep="")
+	if (!file.exists(fname)) {
+		cat("  cannot find input file expected at '",fname,"\n")
+		return(FALSE);
+	}
+
+	#happy(datafile=fname,allelesfile=markers.filename,generations=4,standardise=T,phase="unknown",file.format="happy",missing.code=missing.code)
+	h<-happy(datafile=fname,allelesfile=markers.filename,generations=generations,phase="unknown",file.format="happy",missing.code=missing.code)
+
+	# Every chromosome may have a different set of individuals
+	covariatematrix<-NULL
+	if(!is.null(data.covariates)) {
+		covariatematrix<-as.matrix(read.table.phenotypes[h$subjects,data.covariates,drop=FALSE])
+	}
+
+	fit.0<-hfit(h, permute=0, verbose=verbose, model=model, covariatematrix=covariatematrix)
+	cat("\n",phen,name.suffix,covariates.suffix,"@","all"," fit.0$mapx: ",fit.0$maxp,"\n",sep="")
+
+	fit.permute<-NULL
+	if (permute > 0) {
+		fit.permute<-hfit(h, permute=permute,verbose=TRUE,model=model,covariatematrix=covariatematrix)
+		cat("\n",phen,name.suffix,covariates.suffix,"@","all"," fit.permute$permdata$p01: ",fit.permute$permdata$p01,"\n",sep="")
+		cat(     phen,name.suffix,covariates.suffix,"@","all"," fit.permute$permdata$p05: ",fit.permute$permdata$p05,"\n",sep="")
+	}
+
+	ofile=paste(outputdir,"/","analysis_happy_phen_",phen,name.suffix,"_subset_",individuals.subset,covariates.suffix,"_chr_","together","_model_",model,"_permute_",permute,".pdf",sep="")
+	cat("Writingt PDF to file '",ofile,"'\n") ; pdf(ofile,width=50,height=9)
+	happyplot(fit.0,labels=T,together=TRUE,vlines.chr.lwd=3,vlines.chr.col=vlines.chr.col,
+		main=paste("AIL for phenotype '",phen,"' on subset '",individuals.subset,"'",sep=""),
+		sub=ifelse(is.null(covariatematrix),"No covariates",paste("Covariates ",paste(data.covariates,collapse=",",sep=""))))
+	write.csv(file=paste(outputdir,"/","happy_",individuals.subset,"_",phen, name.suffix,
+		covariates.suffix,"_chr_","together","_maxLodP_",fit.0$maxp,".csv",sep=""), x=fit.0$table)
+	if (!is.null(fit.permute)) {
+		happyplot(fit.permute,labels=TRUE,together=TRUE,
+			main=paste("Permutation data for AIL phenotype",phen),sub=ifelse(is.null(covariatematrix),"No covariates",paste("Covariates ",paste(data.covariates,collapse=",",sep=""))))
+		write.csv(file=paste(outputdir,"/","happy_",individuals.subset,"_",phen,
+				name.suffix,covariates.suffix,"_chr_","together","_maxLodP_",fit.permute$maxp,"_permutation.csv",sep=""),
+			  x=fit.permute$permdata$permutation.pval)
+	}
+	dev.off() ; cat("Created figure at '",ofile,"'\n",sep="")
+	return(TRUE);
+}
+
 
 name.suffix<-ifelse(data.binary,".binary","")
-covariatematrix<-NULL
 
 if (!split.chromosomes) {
 	cat("\n")
@@ -446,66 +503,14 @@ if (!split.chromosomes) {
 
 	#for(phen in colnames(read.table.phenotypes))
 	for(phen in phenotypes) {
-		cat("\n")
-		covariates.suffix<-paste("_covars_",ifelse(is.null(data.covariates),"none",paste(data.covariates,collapse=",",sep="")),sep="")
-		cat("---------- Phenotype '",phen,"' ----- Covariates ",covariates.suffix," -----------------------------------\n",sep="")
 		one.was.omitted<-FALSE
 		for (individuals.subset in c("3m","6m","all")) {
-			if ("weight.6m" %in% data.covariates && "6m" != individuals.subset) {
-				cat("weight.6m specified as covariate, need to work also with that subset.\n")
-				one.was.omitted<-TRUE ; next
-			}
-			if ("weight.3m" %in% data.covariates && "3m" != individuals.subset) {
-				cat("weight.3m specified as covariate, need to work also with that subset.\n")
-				one.was.omitted<-TRUE ; next
-			}
-			
-			cat("Investigating",phen,name.suffix,"at times",individuals.subset,"\n")
-			fname<-paste("happy_",individuals.subset,"_",phen,name.suffix,".input",sep="")
-			if (!file.exists(fname)) {
-				cat("  cannot find input file expected at '",fname,"\n")
-				one.was.omitted<-TRUE ;  next
-			}
 			if ("all"==individuals.subset && one.was.omitted) {
-				cat("There was already some subset not readable/omitted for other reasons. Skipping otherwise missleading 'all'.\n")
+				cat("\nPhnotype ",phen,": There was already some subset not readable/omitted for other reasons. Skipping otherwise missleading 'all'.\n",sep="")
 				next
 			}
-
-			#happy(datafile=fname,allelesfile=markers.filename,generations=4,standardise=T,phase="unknown",file.format="happy",missing.code=missing.code)
-			h<-happy(datafile=fname,allelesfile=markers.filename,generations=generations,phase="unknown",file.format="happy",missing.code=missing.code)
-
-			# Every chromosome may have a different set of individuals
-			if(is.null(data.covariates)) {
-				covariatematrix<-NULL
-			} else {
-				covariatematrix<-as.matrix(read.table.phenotypes[h$subjects,data.covariates,drop=FALSE])
-			}
-
-			fit.0<-hfit(h, permute=0, verbose=verbose, model=model, covariatematrix=covariatematrix)
-			cat("\n",phen,name.suffix,covariates.suffix,"@","all"," fit.0$mapx: ",fit.0$maxp,"\n",sep="")
-
-			fit.permute<-NULL
-			if (permute > 0) {
-				fit.permute<-hfit(h, permute=permute,verbose=TRUE,model=model,covariatematrix=covariatematrix)
-				cat("\n",phen,name.suffix,covariates.suffix,"@","all"," fit.permute$permdata$p01: ",fit.permute$permdata$p01,"\n",sep="")
-				cat(     phen,name.suffix,covariates.suffix,"@","all"," fit.permute$permdata$p05: ",fit.permute$permdata$p05,"\n",sep="")
-			}
-
-			ofile=paste(outputdir,"/","analysis_happy_phen_",phen,name.suffix,"_subset_",individuals.subset,covariates.suffix,"_chr_","together","_model_",model,"_permute_",permute,".pdf",sep="")
-			cat("Writingt PDF to file '",ofile,"'\n") ; pdf(ofile,width=50,height=9)
-			happyplot(fit.0,labels=T,together=TRUE,vlines.chr.lwd=3,vlines.chr.col="darkgray",
-				main=paste("AIL for phenotype '",phen,"' on subset '",individuals.subset,"'",sep=""),
-				sub=ifelse(is.null(covariatematrix),"No covariates",paste("Covariates ",paste(data.covariates,collapse=",",sep=""))))
-			write.csv(file=paste(outputdir,"/","happy_",individuals.subset,"_",phen, name.suffix,
-				covariates.suffix,"_chr_","together","_maxLodP_",fit.0$maxp,".csv",sep=""), x=fit.0$table)
-			if (!is.null(fit.permute)) {
-				happyplot(fit.permute,labels=TRUE,together=TRUE,
-					main=paste("Permutation data for AIL phenotype",phen),sub=ifelse(is.null(covariatematrix),"No covariates",paste("Covariates ",paste(data.covariates,collapse=",",sep=""))))
-				write.csv(file=paste(outputdir,"/","happy_",individuals.subset,"_",phen,
-						name.suffix,covariates.suffix,"_chr_","together","_maxLodP_",fit.permute$maxp,"_permutation.csv",sep=""),
-					  x=fit.permute$permdata$permutation.pval)
-			}
-			dev.off()
+			ok<-analyse.all.chromosomes.together(phen=phen,individuals.subset=individuals.subset,data.covariates=data.covariates,name.suffix=name.suffix)
+			if (!ok) one.was.omitted<-TRUE
 		} #stop("Just stopping to help development.")
 	}
 
