c81c14236796ae7bd7a898a185b40144f654b481
GeorgZeplin <zeplin@informatik.uni-luebeck.de>
2011-04-27 13:58:49 +0200

    warnings

--- website/intergenomics/analysis2.php
+++ website/intergenomics/analysis2.php
@@ -34,7 +34,7 @@ require_once 'utils.php';
 require_once 'fill_related_projects.php';
 fill_compara_array();
 
-$compara = connectToCompara(3306);
+$compara = connectToCompara();
 
 // args
 $projects = array('Ratte', 'Maus');
@@ -49,12 +49,12 @@ $experiment2 = $compara_array[$projects[1]];
 
 // $reg_str = 'regions';
 // $chr2reg = array();
-$species2genome_db_ids = array($experiment1['species'] => $experiment1['genome_db_id'],$experiment2['species']=>$experiment2['genome_db_id']);
-$genome_ids2dbs = array($experiment2['genome_db_id'] => $experiment2['db_name'], $experiment1['genome_db_id'] =>$experiment1['db_name']);
+//$species2genome_db_ids = array($experiment1['species'] => $experiment1['genome_db_id'],$experiment2['species']=>$experiment2['genome_db_id']);
+//$genome_ids2dbs = array($experiment2['genome_db_id'] => $experiment2['db_name'], $experiment1['genome_db_id'] =>$experiment1['db_name']);
 
 
 //load informations (database and so on...)
-$genome_db_ids = array($experiment1['genome_db_id'],$experiment2['genome_db_id']);
+//$genome_db_ids = array($experiment1['genome_db_id'],$experiment2['genome_db_id']);
 $species_names = array($experiment1['species'],$experiment2['species']);
 $database1 = $experiment1['db_name'];
 $database2 = $experiment2['db_name'];
@@ -64,13 +64,13 @@ $dbs = array($database1,$database2);
 $species1 = $experiment1['species'];
 $species2 = $experiment2['species'];
 //  genome db ids
-$genome_id1 = $species2genome_db_ids[$species1];
-$genome_id2 = $species2genome_db_ids[$species2];
+//$genome_id1 = $species2genome_db_ids[$species1];
+//$genome_id2 = $species2genome_db_ids[$species2];
 
 // fetch loci and groups
-$chrs = getChromosomsAndLengths($compara,$experiment1['genome_db_id']);
+$chrs = getChromosomesAndLengths($compara,$experiment1['ensembl_species']);
 // additional filtering
-useDB($genome_ids2dbs[$experiment1['genome_db_id']], $experiment1['connection']);
+useDB($database1, $experiment1['connection']);
 $chrs = filter_chromos($experiment1['connection'], $chrs);
 $chromosomsEx1 = array_keys($chrs);//getChromosoms($compara, $experiment1['genome_db_id']);
 
@@ -84,9 +84,9 @@ $loci_ex1 = array_map('current',$mapEx1);
 $groupnr_ex1 = array_map('next',$mapEx1);
 $loci2group1 = array_combine($loci_ex1, $groupnr_ex1);
 
-$chrs = getChromosomsAndLengths($compara,$experiment2['genome_db_id']);
+$chrs = getChromosomesAndLengths($compara,$experiment2['ensembl_species']);
 // additional filtering
-useDB($genome_ids2dbs[$experiment2['genome_db_id']], $experiment1['connection']);
+useDB($database2, $experiment2['connection']);
 $chrs = filter_chromos($experiment1['connection'], $chrs);
 $chromosomsEx2 = array_keys($chrs);//getChromosoms($compara, $experiment1['genome_db_id']);
 //initialize array for mapping groupnumbers to regions
@@ -100,14 +100,13 @@ $loci_ex2 = array_map('current',$mapEx2);
 
 
 // SYNTENY
-
+$genome_db_ids = getGenomeDBIDs($compara, array($experiment1['ensembl_species'], $experiment2['ensembl_species']));
 $groupSynteny_ex12ex2 = getSyntenyGroups($experiment1['connection'],$compara,$groups1,$groups2,$species_names,$genome_db_ids,$dbs);
 
 // homo
 
 useDB($database1,$experiment1['connection']);
 $loci2stable_ids_ex1 = loci2stable_ids($loci_ex1,$experiment1['connection']);
-$n_qtls1 = 0;
 $unique_ens_ids_ex1 = get_unique_vals_from_2d_array($loci2stable_ids_ex1[0], $n_qtls1);
 
 
@@ -124,7 +123,7 @@ $n_ens_ids_ex2 = sizeof($unique_ens_ids_ex2);
 $traits12traits2 = array();
 //$cnt_homo = array();
 if($n_ens_ids_ex1 < $n_ens_ids_ex2){// homology on experiment 1
-	$homology_ex1 = get_homologue_ens_ids($compara,$unique_ens_ids_ex1,$genome_id2);
+	$homology_ex1 = get_homologue_ens_ids($compara,$unique_ens_ids_ex1,$experiment2['ensembl_species']);
 	//intersection
 	foreach ($homology_ex1 as $unique_id_ex1 => $corr_homologue_ens_ids_ex2) {
 		$intersect = array_intersect(array_keys($corr_homologue_ens_ids_ex2),
@@ -135,7 +134,7 @@ if($n_ens_ids_ex1 < $n_ens_ids_ex2){// homology on experiment 1
 		}
 	}
 }else{
-	$homology_ex2 = get_homologue_ens_ids($compara,$unique_ens_ids_ex2,$genome_id1);
+	$homology_ex2 = get_homologue_ens_ids($compara,$unique_ens_ids_ex2,$experiment1['ensembl_species']);
 	//intersection
 
 	foreach ($unique_ens_ids_ex1 as $id_ex1){
@@ -212,6 +211,12 @@ foreach ($loci2stable_ids_ex1 as $locus1 => $stables1) {
 		}
 	}
 }
-
+echo <<<END
+$qtl_sh1 <br>
+$qtlsh1 <br>
+$qtl_s1 <br>
+$qtl_h1 <br>
+$qtl_n1 <br>
+END;
 include 'html/footer.html';
 ?>
